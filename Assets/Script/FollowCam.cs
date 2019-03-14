a1b709c8eb919aefc9820aa543fcea16a64b10b9
Defeng Kong <vergildfkong@hotmail.com>
2019-03-14 18:32:35 -0400

    ··

---
+++ Assets/Script/FollowCam.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+
+    static public GameObject POI; // The static point of interest                // a
+    [Header("Set Dynamically")]
+    public float camZ; // The desired Z pos of the camera
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+    void FixedUpdate()
+    {
+        // if there's only one line following an if, it doesn't need braces
+        //if (POI == null) return; // return if there is no poi                   // b
+        // Get the position of the poi
+        // Vector3 destination = POI.transform.position;// Force destination.z to be camZ to keep the camera far enough away
+        Vector3 destination;
+        // If there is no poi, return to P:[ 0, 0, 0 ]
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            // Get the position of the poi
+            destination = POI.transform.position;
+            // If poi is a Projectile, check to see if it's at rest
+            if (POI.tag == "Projectile")
+            {
+                // if it is sleeping (that is, not moving)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // return to default view
+                    POI = null;
+                    // in the next update
+                    return;
+                }
+            }
+        }
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
+
+        destination.z = camZ;// Set the camera to the destination
+
+        transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Slingshot.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    static private Slingshot S;
+
+    [Header("Set in Inspector")]                                            // a
+    public GameObject prefabProjectile;
+    public float velocityMult = 8f;
+    // fields set dynamically
+    [Header("Set Dynamically")]                                             // a
+    public GameObject launchPoint;
+    public Vector3 launchPos;                                   // b
+    public GameObject projectile;                                  // b
+    public bool aimingMode;                                  // b
+    private Rigidbody projectileRigidbody;                             // a
+    static public Vector3 LAUNCH_POS
+    {                                        // b
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
+    void Awake()
+    {
+        S = this;
+
+        Transform launchPointTrans = transform.Find("LaunchPoint");              // a
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+
+    }
+
+    void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);                                           // b
+
+    }
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);                                           // b
+
+    }
+
+    void OnMouseDown()
+    {                                                    // d
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        // Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Start it at the launchPoint
+        projectile.transform.position = launchPos;
+        // Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();                // a
+        projectileRigidbody.isKinematic = true;                                    // a
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!aimingMode) return;                                                   // b
+                                                                                   // Get the current mouse position in 2D screen coordinates
+        Vector3 mousePos2D = Input.mousePosition;                                  // c
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        // Find the delta from the launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        // Limit mouseDelta to the radius of the Slingshot SphereCollider          // d
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        {                                         // e
+            // The mouse has been released
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+
+            projectile = null;
+            MissionDemolition.ShotFired();                             // a
+            ProjectileLine.S.poi = projectile;                         // b
+
+        }
+
+    }
+}
